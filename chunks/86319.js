a3525f5086c78f910913814799e004d5ab5f04b5
DiscrapperManager <admin@minerpl.xyz>
2024-01-07 13:29:38 +0100

    canaryRelease: 257704

---
+++ chunks/86319.js
@@ -0,0 +1,17 @@
+function(e, n, i) {
+    "use strict";
+    i.r(n), i.d(n, {
+        default: function() {
+            return a
+        }
+    });
+    var t = i("559610"),
+        o = i("63173");
+
+    function a(e, n, i) {
+        (0, t.default)(2, arguments);
+        var a = (0, o.default)(e, i),
+            r = (0, o.default)(n, i);
+        return a.getTime() === r.getTime()
+    }
+}
