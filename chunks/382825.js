69f4e6f693882ecd1cdb2559a7f391fe0a66e7ef
DiscrapperManager <admin@minerpl.xyz>
2024-01-17 22:16:15 +0100

    canaryRelease: 259501

--- chunks/382825.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-var e, a;
-I.r(E), I.d(E, {
-  TwoWayLinkType: function() {
-    return e
-  }
-}), I("222007"), (a = e || (e = {})).MOBILE = "mobile", a.DESKTOP = "desktop", a.WEB = "web", a.DEVICE_CODE = "device_code"
